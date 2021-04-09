4d70292a22f52eb9be5f01d4aa5a04c0e09d7f20
schirmann <cschirmann.dev@gmail.com>
2021-04-09 16:38:54 -0300

    inserindo cronometro

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
 
 export default class cronometro extends Component {
   render() {
     return (
       <View style={styles.body}>
         <Image source={require("./images/relogio.png")} />
+        <Text style={styles.timer}>0.0</Text>
       </View>
     );
   }
@@ -18,5 +19,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#ef5c6c",
+  },
+  timer: {
+    color: "#c6a46d",
+    fontSize: 80,
+    fontWeight: "bold",
+    backgroundColor: "transparent",
+    marginTop: -150,
   },
 });
